d6d77da717a61e9d178d1810d046e9743d0c4159
goutham-arkl <gouthemarkl@gmail.com>
2023-04-22 15:15:40 +0530

    courosel animation

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -2,6 +2,8 @@ import React,{useState} from 'react'
 import './Carousel.css'
 import Carousel1 from '../../images/Carousel1.jpg'
 import Carousel2 from '../../images/Carousel2.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGreaterThan, faLessThan,faPlay } from '@fortawesome/free-solid-svg-icons';
 
 const Carousel = () => {
     const images = [
@@ -23,9 +25,22 @@ const Carousel = () => {
   
     return (
       <div className="slider" style={{backgroundImage:`url(${images[currentImageIndex]})`}}>
-        <button onClick={handlePrevious}>Previous</button>
+        <button className='prev' onClick={handlePrevious}><FontAwesomeIcon icon={faLessThan}/></button>
+
+        <div className='carousel-data'>
+            <div className='carousel-headings'>
+                <span className='social-life'>Great Quality Cocial life</span>
+                <h2 className='carousel-title'>Discover the world of possible university</h2>
+                <button className='admission-btn'>Admissions</button>
+            </div>
+            <div className='video-div'>
+                <div className='anime'>
+                <button className='video-btn'><FontAwesomeIcon icon={faPlay}/></button>
+                </div>
+            </div>
+        </div>
         
-        <button onClick={handleNext}>Next</button>
+        <button className='next' onClick={handleNext}><FontAwesomeIcon icon={faGreaterThan}/></button>
       </div>
     );
   };
